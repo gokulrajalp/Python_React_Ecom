476cfd49e8bfe443516551dbf4f685c1ab710439
gokulrajalp <gokulrajalp@gmail.com>
2024-04-23 03:08:31 +0530

    first commit

--- src/componrnts/Home.js
+++ src/componrnts/Home.js
@@ -1,45 +1,106 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../assets/css/home.css";
 
 import { Scrollbars } from "react-custom-scrollbars-2";
 
 const Home = () => {
-  const data = [
+  const [data, setData] = useState ([
     {
-      imgFileName: "arabian-ranches-ii.jpg",
-      title: "Arabian Ranches",
-      description:
-        "Arabian Ranches offers a modern interpretation of the soothing charm of the surrounding desertscape. The development features a number of residential communities that draw inspiration from Spanish and Arabian architecture.",
+      "categoryName": "category 1",
+      "categoryId": 1,
+      "quantity" : 0,
+      "totalPrice":0,
+      "products": [
+        {
+          "id":1,
+          "imgFileName": "dubai-marina.png",
+          "title": "Product 1",
+          "price": 2,
+          "description": "Arabian Ranches offers a modern interpretation...",
+          "quantity" : 0,
+          "totalPrice":0,
+        },
+        {
+          "id":2,
+          "imgFileName": "dubai-marina.png",
+          "title": "Product 2",
+          "price": 10,
+          "description": "Arabian Ranches offers a modern interpretation...",
+          "quantity" : 0,
+          "totalPrice":0,
+        },
+        {
+          "id":3,
+          "imgFileName": "dubai-marina.png",
+          "title": "Product 3",
+          "price": 100,
+          "description": "The Centre of Now. The most prestigious square...",
+          "quantity" : 0,
+          "totalPrice":0,
+        }
+      ]
     },
     {
-      imgFileName: "arabian-ranches.jpg",
-      title: "Arabian Ranches II",
-      description:
-        "Arabian Ranches offers a modern interpretation of the soothing charm of the surrounding desertscape. The development features a number of residential communities that draw inspiration from Spanish and Arabian architecture.",
-    },
+      "categoryName": "category 2",
+      "categoryId": 2,
+      "quantity" : 0,
+      "totalPrice":0,
+      products: [
+        {
+          "id":4,
+          "imgFileName": "dubai-marina.png",
+          "title": "Product 4",
+          "price": 50,
+          "description": "Sustainably designed, Dubai Hills Estate is...",
+          "quantity" : 0,
+          "totalPrice":0,
+        },
+        {
+          "id":5,
+          "imgFileName": "dubai-marina.png",
+          "title": "Product 5",
+          "price": 500,
+          "description": "Dubai Marina is one of the world’s largest...",
+          "quantity" : 0,
+          "totalPrice":0,
+        },
+        {
+          "id":6,
+          "imgFileName": "dubai-marina.png",
+          "title": "Product 6",
+          "price": 1000,
+          "description": "Launched in 2003, Emirates Living is a...",
+          "quantity" : 0,
+          "totalPrice":0,
+        }
+      ]
+    }
+  ]);
+  
+  const banner = [
     {
-      imgFileName: "downtown-dubai.jpg",
-      title: "Downtown Dubai",
-      description:
-        "The Centre of Now. The most prestigious square kilometre in the world. Downtown Dubai is no stranger to such accolades, and yet it’s hard to overstate the prominence of this community.",
-    },
-    {
-      imgFileName: "dubai-hills-estate.jpg",
-      title: "Dubai Hills Estate",
-      description:
-        "Sustainably designed, Dubai Hills Estate is a first of its kind destination. This masterfully-planned 2,700-acre multi-purpose development will form an integral part of the Mohammed Bin Rashid City.",
+      imgFileName: "dubai-marina.png",
+      title: "Service 1",
     },
     {
       imgFileName: "dubai-marina.png",
-      title: "Dubai Marina",
-      description:
-        "Dubai Marina is one of the world’s largest, most meticulously planned waterfront developments and offers the exhilaration and vibrancy of a chic, urban lifestyle together with all the advantages of living on the water.",
+      title: "Service 2",
     },
     {
-      imgFileName: "emirates-living.jpg",
-      title: "Emirates Living",
-      description:
-        "Launched in 2003, Emirates Living is a modern lifestyle community focused on outdoor leisure. Emirates Living offers a serene nature-filled sanctuary, with 8,659 premium villas nestled within 52.2 million square feet of lush greenery.",
+      imgFileName: "dubai-marina.png",
+      title: "Service 3",
+    },
+    {
+      imgFileName: "dubai-marina.png",
+      title: "Service 4",
+    },
+    {
+      imgFileName: "dubai-marina.png",
+      title: "Service 5",
+    },
+    {
+      imgFileName: "dubai-marina.png",
+      title: "Service 6",
     },
   ];
 
@@ -50,36 +111,115 @@ const Home = () => {
     setStartIndex((prevIndex) => (prevIndex + 3) % data.length);
   };
 
+ const [res, setRes] = useState({"grandQuantity":0, "grandTotal":0})
+
+useEffect(()=>{
+  let grandQuantity = 0;
+  let grandTotal = 0;
+  data.forEach((category)=>{
+    grandQuantity += category.quantity
+    grandTotal += category.totalPrice
+  })
+  setRes({"grandQuantity":grandQuantity, "grandTotal":grandTotal})
+},[data])
+
+  const updateProduct = (categoryId, productId, quantity) => {
+
+   
+   setData(prevData => {
+      const newData = [...prevData];
+      
+  
+      newData.forEach(category => {
+        if (category.categoryId && category.categoryId === categoryId) {
+          category.products.forEach(product => {
+            if (product.id === productId) {
+              product.quantity = quantity;
+              product.totalPrice = quantity * product.price;
+            }
+          });
+  
+          category.quantity = category.products.reduce((acc, curr) => acc + parseInt(curr.quantity), 0);
+          category.totalPrice = category.products.reduce((acc, curr) => acc + curr.totalPrice, 0);
+          
+         
+        }
+      });
+  
+      return newData;
+    });
+ 
+  };
+  
+
+
+
   return (
     <div>
+    <nav class="navbar navbar-light bg-light sticky-top">
+  <a class="navbar-brand" href="#">
+    <img src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp" height="20" alt="MDB Logo" loading="lazy" />
+  </a>
+  <div>
+      <button type="button" className="btn btn-outline-primary border me-3">
+        Quantity: {res.grandQuantity}
+      </button>
+      <button type="button" className="btn btn-outline-primary border">
+        Total price: ₹{res.grandTotal}
+      </button>
+    </div>
+  <div></div>
+</nav>
+
       <div className="container">
-        <h1 className="text-center m-4">COMMUNITIES WE MANAGE</h1>
+        <h1 className="text-center m-4 mb-0">Available Products</h1>
 
-        <div className="d-flex justify-content-center align-items-center">
-          <div className="row g-5">
-            {data.map((item, index) => (
-              <div key={index} className="col-lg-4 col-md-6">
-                <div className="card rounded-0">
-                  <img
-                    src={require(`../assets/images/${item.imgFileName}`)}
-                    className="card-img-top rounded-0"
-                    alt="Card image"
-                    style={{ height: "250px" }}
-                  />
-                  <div className="card-body">
-                    <div className="card-text-container">
-                      <p className="card-text">{item.description}</p>
-                    </div>
+        
+    {data.map((category) => (
+  <div key={category.categoryId}>
+    {category.categoryName && <button type="button" className="btn btn-outline-primary mb-3 mt-5" style={{ width: "100%" }}>
+      {category.categoryName}
+    </button>}
+    <div className="d-flex justify-content-center align-items-center">
+      <div className="row g-5">
+        {category.products && category.products.map((item) => (
+          <div key={item.id} className="col-lg-4 col-md-6">
+            <div className="card rounded-0">
+              <img
+                src={require(`../assets/images/${item.imgFileName}`)}
+                className="card-img-top rounded-0"
+                alt="Card image"
+                style={{ height: "250px" }}
+              />
+              <div className="card-body">
+                <div className="card-text-container">
+                  <p className="card-text">{item.title}</p>
+                  <div className="d-flex justify-content-between">
+                    <p className="card-text">Actual Price : <span className="text-decoration-line-through text-danger">{item.price}</span></p>
+                    <p className="card-text" style={{ fontWeight: 'bold'}}>Discount Price : <span className="text-success">{item.price}</span></p>
                   </div>
-
-                  <div className="card-title-container">
-                    <h5 className="card-title">{item.title}</h5>
-                  </div>
+                  {item.description && <div className="scrollable-container" style={{maxHeight: '4rem', overflowY: 'scroll', }}>
+                    <p className="card-text" style={{marginRight: '17px'}}>
+                      {item.description}
+                    </p>
+                  </div>}
                 </div>
               </div>
-            ))}
+
+              <div className="d-flex justify-content-between m-2 align-items-center">
+                <button type="button" className="btn btn-primary" style={{ fontWeight: 'bold'}} onClick={() => updateProduct(category.categoryId, item.id, (item.quantity-1))}>-</button>
+                <input type="number" className="form-control" value={item.quantity} style={{ width: "100px" }} onChange={(e) => updateProduct(category.categoryId, item.id, e.target.value)}/>
+                <button type="button" className="btn btn-primary" style={{ fontWeight: 'bold'}} onClick={() =>updateProduct(category.categoryId, item.id, (item.quantity+1))}>+</button>
+                <p className="card-text">Total Price : {item.totalPrice}</p>
+              </div>
+            </div>
           </div>
-        </div>
+        ))}
+      </div>
+    </div>
+  </div>
+))}
+
 
         <h1
           className="text-center"
@@ -93,7 +233,7 @@ const Home = () => {
           style={{ marginBottom: "20px" }}
         >
           <div className="row">
-            {data.slice(startIndex, startIndex + 3).map((item, index) => (
+            {banner.slice(startIndex, startIndex + 3).map((item, index) => (
               <div key={index} className="col-md-4">
                 <img
                   src={require(`../assets/images/${item.imgFileName}`)}
@@ -151,7 +291,7 @@ const Home = () => {
         <Scrollbars>
           <div className="d-md-none container-fluid p-0">
             <div className="d-flex flex-row flex-nowrap">
-              {data.map((item, index) => (
+              {banner.map((item, index) => (
                 <div key={index} className="mr-2">
                   <img
                     src={require(`../assets/images/${item.imgFileName}`)}
@@ -180,6 +320,9 @@ const Home = () => {
           </div>
         </Scrollbars>
       </div>
+      <div className="fixed-bottom d-flex justify-content-end p-3">
+      <button type="button" className="btn btn-primary">Proceed to Buy</button>
+    </div>
     </div>
   );
 };
