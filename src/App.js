5446efc24269770ef819e821e3ad043bcd0db5d3
gokulrajalp <gokulrajalp@gmail.com>
2024-05-07 08:59:27 +0530

    Upload updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './componrnts/Home';
-import AddProduct from './componrnts/AddProduct';
+import Home from './components/Home';
+import AddProduct from './components/AddProduct';
 
 function App() {
   return (
